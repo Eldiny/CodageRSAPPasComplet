24b036f59f5b9f7395b488316ad015131914cc84
Eldiny <alexandre.spinace@icloud.com>
2026-01-05 15:22:23 +0100

    Add files via upload

--- CodageRSA.py
+++ CodageRSA.py
@@ -2,6 +2,9 @@ from random import randint
 from math import sqrt
 
 def PGCD (nombreun, nombredeux) :
+    #IN : Deux nombres
+    #OUT : Leur PGCD
+
     a = 0
     PGCD2 = 1
     if nombreun > nombredeux :
@@ -14,6 +17,10 @@ def PGCD (nombreun, nombredeux) :
     return(PGCD2)
 
 def Grand (nombre, N) :
+    #IN : Un nombre
+    #OUT : Un nommbre premier avec ce dernier
+    # Tant que faux, teste des nombres aleatoires entre 1000 et 10000 jusqu'à qu'un soit premier avec le nombre
+
     a = True
     d = 0
     while a == True :
@@ -23,6 +30,10 @@ def Grand (nombre, N) :
     return(d)
 
 def GranbdNombresAleatoires () :
+    #IN : Rien
+    #OUT : 2 nombres premiers entre 1000 et 10000
+    #Teste des nombres aléatoires entre 1000 et 10000 jusqu'à en trouver 2 qui soient premiers
+
     liste = []
     d = 0
     while len(liste) < 2 :
@@ -32,6 +43,10 @@ def GranbdNombresAleatoires () :
     return(liste[0], liste[1])
 
 def Premier(nombre) :
+    #IN : Un nombre
+    #OUT : La vérification de s'il est premier
+    #Regarde les diviseurs jusqu'à sa racine et s'il n'y en a pas alors il est premier
+
     if nombre < 2 :
         return(False)
     for i in range(2, int(sqrt(nombre)+1)) :
@@ -40,6 +55,10 @@ def Premier(nombre) :
     return(True)
 
 def CoeffDeBezout(nombreun, nombredeux) :
+    #IN : Deux nombres
+    #OUT : Leurs coefficients de Bezout
+    #Fais les trucs mathematiques bizarres pour les trouver puis les renvoie
+
     if nombreun > nombredeux :
         b = nombredeux
         a = nombreun
@@ -62,6 +81,11 @@ def CoeffDeBezout(nombreun, nombredeux) :
     return(u, v)
 
 def Decoupe(mot) :
+    #IN : Une phrase
+    #OUT : La phrase découpée en bloc de 3 maximum et retranscrite en chiffres
+    #Fais un dico qui permet de retranscrir les lettres en chiffres, fais deux listes pour vérifier les lettres majuscules et minuscules et seulement ces dernieres. Puis leur attribue leurs valeurs respectivese en
+    #leur rajoutant un 0 devant si nécessaire. Puis renvoie les valeurs des blocs de 3
+
     m = 0
     dico = {}
     for n in range (97,123) :
@@ -89,9 +113,15 @@ def Decoupe(mot) :
             compte = 0
             CodeX.append(part)
             part = ""
+    if part != "" :
+        CodeX.append(part)
     return(CodeX)
 
 def Crypter(Code, N, e) :
+    #IN : Une liste et 2 nombres
+    #OUT : Une liste
+    #Prend le code, prend chacun des nombres à l'intérieur et les multiplie comme il faut pour les coder, puis renvoie une liste contenant les nombres codées
+
     Crypté = 0
     TrucFinal = []
     for i in Code :
@@ -100,6 +130,10 @@ def Crypter(Code, N, e) :
     return(TrucFinal)
 
 def Decrypter(Code, N, d) :
+    #IN : Une liste et 2 nombres
+    #OUT : Une liste
+    #Prend le code crypté, prend chacun des nombres et leur fait l'opération pour décrypter puis les ajoute à une liste avant de la renvoyer
+
     Crypté = 0
     TrucFinal = []
     for i in Code :
@@ -107,23 +141,50 @@ def Decrypter(Code, N, d) :
         TrucFinal.append(Crypté)
     return(TrucFinal)
 
-p, q = GranbdNombresAleatoires()
-print(p,q)
+def Retrouve(Code) :
+    #IN : Une liste
+    #OUT : Une liste contenant chaque caractère décodé
+    #Crée un dico pour retrouver les lettres associées aux chiffres puis retrouve le message.
+
+    m = 0
+    a = 0
+    dico = {}
+    for n in range (97,123) :
+        dico[m] = chr(n)
+        m+=1
+    BonneForme = []
+    TrucFinal = []
+    for e in Code :
+        Longueur = 0
+        for i in str(e) :
+            Longueur += 1
+        if Longueur % 2 == 1 :
+            BonneForme.append("0"+str(e))
+        else :
+            BonneForme.append(str(e))
+    for e in BonneForme :
+        compte = 1
+        for b in e :
+            a += int(b)*(10**compte)
+            compte -= 1
+            if compte == -1 :
+                TrucFinal.append(dico[a])
+                compte = 1
+                a = 0
+    return(TrucFinal)
+
+
+p, q = GranbdNombresAleatoires() #Détermine les clés publiques
 N = p*q
 N2 = (p-1)*(q-1)
-print(N2)
-e = Grand(N2, N)
-k, d = CoeffDeBezout(N2, e)
-print(d)
+e = Grand(N2, N) #Détermine e en fonction de N et N2
+k, d = CoeffDeBezout(N2, e) #Trouve les coeff de Bezout de N2 et eB
 
-euh = Decoupe('Bonjour a tous')
-oué = (Crypter(euh, N, e))
-print(euh)
-print(oué)
-print(Decrypter(oué, N, d))
-
-m = 0
-dico = {}
-for n in range (97,123) :
-    dico[chr(n)] = m
-    m+=1
+Message = str(input("Quelle message voulez vous codez? Seulement les lettres en minuscule et majuscules sans accents seront transcrites : "))
+Decoupee = Decoupe(Message)
+print(Decoupee)
+Crypte = (Crypter(Decoupee, N, e))
+print("Voici votre message crypté :", Crypte)
+Decrypte = (Decrypter(Crypte, N, d))
+print("Voici votre message décrypté :", Decrypte)
+print("Votre message une fois retranscris donne :",Retrouve(Decrypte))
